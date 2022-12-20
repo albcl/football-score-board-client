cd2805bf25befb1d1a2fbdc0d9d2ba32e9df393b
albcl <a.cerveto@gmail.com>
2022-12-20 23:46:19 +0100

    Will input, add and return a match

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,13 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('App Cases', () => {
     test('Renders just fine', () => {
         expect(() => <App />).not.toThrowError();
     });
+
     test('Renders just fine and find elements', () => {
         render(<App />);
         const h1 = screen.getByText(/live score board/i);
         expect(h1).toBeInTheDocument();
+    });
+
+    test('Receive and display matches', async () => {
+        render(<App />);
+        const homeInput = screen.getByLabelText('Home Team');
+        const awayInput = screen.getByLabelText('Away Team');
+
+        fireEvent.change(homeInput, { target: { value: 'team 01' } });
+        fireEvent.change(awayInput, { target: { value: 'team 02' } });
+        fireEvent.click(screen.getByText(/submit/i));
+
+        expect(await screen.findByText(/team 01 0/)).toBeInTheDocument();
+        expect(await screen.findByText(/team 02 0/)).toBeInTheDocument();
     });
 });
