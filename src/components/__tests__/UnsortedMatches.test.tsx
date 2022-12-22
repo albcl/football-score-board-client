d342b81ac1089bf12ba7bde71cfe4f9d5b89c0c5
albcl <a.cerveto@gmail.com>
2022-12-22 12:28:43 +0100

    Will display a list of playing games from Context

--- src/components/__tests__/UnsortedMatches.test.tsx
+++ src/components/__tests__/UnsortedMatches.test.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import { useContext } from 'react';
+import { FormContext } from '../context';
 import UnsortedMatches from '../UnsortedMatches';
 
 describe('UnsortedMatches Cases', () => {
@@ -8,4 +10,36 @@ describe('UnsortedMatches Cases', () => {
         expect(screen.getByText(/live matches/i)).toBeInTheDocument();
     });
 
+    test('Will get its data from Context', () => {
+        const mockValue = {
+            ...defaultContextValues,
+            playingMatches: {
+                "['Team 01','Team 02']": { teams: ['Team 01', 'Team 02'], score: [0, 0] },
+            },
+        };
+
+        const TestingComponent = () => {
+            const { playingMatches } = useContext(FormContext);
+            return (
+                <ul>
+                    {Object.entries(playingMatches).map(([key, value]) => {
+                        return (
+                            <p key={key}>
+                                {key} - {value.teams.join(' ')} - {value.score.join(' ')}
+                            </p>
+                        );
+                    })}
+                </ul>
+            );
+        };
+
+        render(
+            <FormContext.Provider value={mockValue}>
+                <TestingComponent />
+            </FormContext.Provider>,
+        );
+
+        expect(screen.getByText(/team 01 team 02/i)).toBeInTheDocument();
+        expect(screen.getByText(/0 0/i)).toBeInTheDocument();
+    });
 });
