495cee9f27cf0f80f7757e6e19ec6c46e27b8eeb
albcl <a.cerveto@gmail.com>
2022-12-22 12:42:38 +0100

    Provider updated to store "playingMatches"

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -4,18 +4,21 @@ import { FormProviderTypes, NewMatchTypes, PlayingMatchesTypes } from './types';
 
 function FormProvider({ board, children }: FormProviderTypes) {
     const [newMatch, setNewMatch] = useState<NewMatchTypes>({ home: '', away: '' });
+    const [playingMatches, setPlayingMatches] = useState<PlayingMatchesTypes>({});
     const [liveScores, setLiveScores] = useState<string[]>([]);
     const [error, setError] = useState<string>('');
 
     const refreshLiveScores = () => setLiveScores(board.getLiveSummary());
+    const addPlayingMatch = (key: string, teams: string[], score = [0, 0]) =>
+        setPlayingMatches({ ...playingMatches, [key]: { teams: teams, score: score } });
 
     const provider = {
         error,
         liveScores,
         newMatch,
-        handleFormChange: (e: React.ChangeEvent<HTMLInputElement>, key: string) => {
-            setNewMatch({ ...newMatch, [key]: e.target.value });
-        },
+        playingMatches,
+        handleFormChange: (e: React.ChangeEvent<HTMLInputElement>, key: string) =>
+            setNewMatch({ ...newMatch, [key]: e.target.value }),
         handleSubmit: (e: React.FormEvent) => {
             e.preventDefault();
             if (error) setError('');
@@ -25,6 +28,7 @@ function FormProvider({ board, children }: FormProviderTypes) {
                 .addMatch(match)
                 .then(() => {
                     refreshLiveScores();
+                    addPlayingMatch(JSON.stringify(match), match);
                 })
                 .catch((error: { [key: string]: string }) => setError(error.message));
         },
