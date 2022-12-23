1de8594a734f3f50804312219112b41b0112457f
albcl <a.cerveto@gmail.com>
2022-12-23 04:26:08 +0100

    Moved Board components into its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import './App.css';
-import { useContext } from 'react';
-import FormContext from './context/formContext';
-import WithBoard from './HOC/WithBoard';
-import SortedMatches from './components/SortedMatches';
-import AddMatchForm from './components/AddMatchForm';
+import LiveScoreBoard from './components/LiveScoreBoard';
 
-function App() {
-    const { error, liveScores } = useContext(FormContext);
+const App = () => (
+    <div className='App'>
+        <h1>Football Live Score Board</h1>
+        <hr />
+        <LiveScoreBoard />
+    </div>
+);
 
-    return (
-        <div className='App'>
-            <h1>Football Live Score Board</h1>
-            <hr />
-            <AddMatchForm />
-            {error && (
-                <p role='alert' id='error-message' style={{ color: 'red' }}>
-                    {error}
-                </p>
-            )}
-            <SortedMatches data={liveScores} />
-        </div>
-    );
-}
-export default WithBoard(App);
+export default App;
---
+++ src/components/LiveScoreBoard/index.tsx
@@ -0,0 +1,47 @@
+import React, { useContext } from 'react';
+
+import SortedMatches from '../SortedMatches';
+
+import UnsortedMatches from '../UnsortedMatches';
+import AddMatchForm from '../AddMatchForm';
+import FormContext from '../../context/formContext';
+import WithBoard from '../../HOC/WithBoard';
+
+/**
+ * Main scores boarc where it connects transparently to the
+ * FormContext provider and Board() from `football-score-board`
+ * It makes use of *{liveScores, error}* from *FormContext*
+ */
+const LiveScoreBoard = () => {
+    const { liveScores, error } = useContext(FormContext);
+
+    return (
+        <React.Fragment>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                <AddMatchForm />
+
+                {error && (
+                    <p role='alert' id='error-message' style={{ color: 'red' }}>
+                        {error}
+                    </p>
+                )}
+
+                <div style={{ display: 'flex', justifyContent: 'space-evenly', width: '100%' }}>
+                    <div style={{ margin: '0 2rem' }}>
+                        <UnsortedMatches />
+                    </div>
+                    <div style={{ margin: '0 2rem' }}>
+                        <SortedMatches data={liveScores} />
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default WithBoard(LiveScoreBoard);
