f5b36709bd2307bd3d83b2115002aa1bac746ad7
albcl <a.cerveto@gmail.com>
2022-12-23 05:31:31 +0100

    Better naming for HOC `<WithBoard />` types

--- src/HOC/WithBoard.tsx
+++ src/HOC/WithBoard.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Board } from 'football-score-board';
 import FormProvider from '../context';
 
-interface AwaysCoolStateProps {
+type WithBoardTypes = {
     board: Board;
-}
+};
 
+/**
+ * High Order Component to wrap your component with FormProvider and initialize a new Board()
+ */
 function WithBoard(WrappedComponent: React.ComponentType<any | string>) {
-    return class Component extends React.Component<{}, AwaysCoolStateProps> {
+    return class Component extends React.Component<{}, WithBoardTypes> {
         render() {
             return (
                 <FormProvider board={new Board()}>
